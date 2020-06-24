35ed7d81bbe44e9d2e04dc8f84d23ab882005033
unknown <rodrigo170@hotmail.com>
2020-06-24 14:34:13 -0300

    empece a trabajar en la navegacion

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import {Route, Switch} from 'react-router-dom';
 import Home from './Components/Home';
 import Logo from './img/Logo.png';
 import Background from './img/background.png'
@@ -12,7 +13,27 @@ render() {
       <div className="col-12"> 
         <img className="img-fluid mx-auto d-block" src={Logo} alt="logo"/>          
       </div>
-      <Home/>
+      {/* <div className="col-12">
+      <ul className="nav justify-content-center">
+        <li className="nav-item">
+          <a className="nav-link active" href="#">Active</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Link</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Link</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Link</a>
+        </li>
+      </ul>
+      </div> */}
+      <Switch>
+        <Route exact path="/" component={SignUp} />
+        <Route path="/login" component={Login} exact />
+        <Route path="/home" component={Home} exact />        
+      </Switch>
     </div>
   </div>
   );
--- src/Components/Character.js
+++ src/Components/Character.js
@@ -3,9 +3,9 @@ import React from "react";
 const Character = ({name, birth_year, hair_color, eye_color, mass}) => {
     return (
       <div className = "col-4">
-      <div className="card">
-        <div className="card-body">
-          <h5 className="card-title text-white">Name: {name}</h5>
+      <div className="card my-2">
+        <div className="card-body text-center">
+          <h5 className="card-title text-primary">{name}</h5>
           <p className="card-text">Birth Year: {birth_year}</p>
           <p className="card-text">Hair Color: {hair_color}</p>
           <p className="card-text">Eye Color: {eye_color}</p>
