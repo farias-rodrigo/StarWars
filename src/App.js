19d12553eafc549e122941832dfb54755bad9385
unknown <rodrigo170@hotmail.com>
2020-06-26 13:43:05 -0300

    casi pronto el login con usuarios locales

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
-import {Route, Switch} from 'react-router-dom';
+import {Route, Switch, Link} from 'react-router-dom';
 import Home from './Components/Home';
+import Login from './Components/Login'
 import Logo from './img/Logo.png';
 import Background from './img/background.png'
 
@@ -13,22 +14,22 @@ render() {
       <div className="col-12"> 
         <img className="img-fluid mx-auto d-block" src={Logo} alt="logo"/>          
       </div>
-      {/* <div className="col-12">
+      <div className="col-12">
       <ul className="nav justify-content-center">
         <li className="nav-item">
-          <a className="nav-link active" href="#">Active</a>
+          <Link className="nav-link active" to="#">Active</Link>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">Link</a>
+          <Link className="nav-link" to="#">Link</Link>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">Link</a>
+          <Link className="nav-link" to="#">Link</Link>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">Link</a>
+          <Link className="nav-link" to="#">Link</Link>
         </li>
       </ul>
-      </div> */}
+      </div>
       <Switch>
         <Route exact path="/" component={SignUp} />
         <Route path="/login" component={Login} exact />
---
+++ src/Components/Login.js
@@ -0,0 +1,73 @@
+import React, {useState} from "react";
+import { withRouter, Link } from "react-router-dom";
+
+const Login = ({history}) =>{
+    const [state, setState] = useState({
+        userName:'',
+        password: '',
+        message: '',
+        users:[]
+    })
+    const {userName, password, message, users} = state;
+
+    const handleInputChange = ({target: {name, value}}) =>{
+        setState({
+            ...state,
+            [name]: value
+        })
+    }
+
+    const handleSubmit = event =>{
+        event.preventDefaul();
+        users.forEach(u => { 
+            if(u.userName == userName.toLowerCase && u.password == password.toLowerCase){    
+
+                sessionStorage.setItem("isLogged",1);
+                history.push("/Home");               
+            }else{
+                setState({
+                    ...state,
+                    message:"LogIn Failed! Please, check your UserName and / or your Password"
+                })
+            }
+            
+        });
+    }
+    return(
+        <div className="row">
+      <div className="col-6 text-center mx-auto">
+        <h2 className="text-center">Sign In</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="col-6 mx-auto my-2">
+            <input
+              className="mx-auto text-center"
+              type="userName"
+              name="userName"
+              placeholder="User Name"
+              value={userName}
+              onChange={handleInputChange} 
+              required
+            />
+          </div>
+          <div className="col-6 mx-auto my-1">
+            <input
+              className="mx-auto text-center"
+              type="password"
+              name="password"
+              placeholder="Password"
+              value={password}
+              onChange={handleInputChange} 
+              required
+            />
+          </div>
+          <input type="submit" value="Sign In" />
+          {message && <h2 className="text-danger">{message}</h2>}
+        </form>
+        <Link to="/">Or Sign Up</Link>
+      </div>
+    </div>
+    );
+
+}
+
+export default withRouter(Login);
