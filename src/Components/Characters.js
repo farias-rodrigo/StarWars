0149a2c7775fb856ee897f2f851447250d848477
unknown <rodrigo170@hotmail.com>
2020-06-29 13:38:18 -0300

    armando el home

--- src/Components/Characters.js
+++ src/Components/Characters.js
@@ -14,37 +14,33 @@ const Characters = () =>{
 
     useEffect(() => {
         async function fetchPeople(){
-            let res = await fetch("https://swapi.dev/api/people");
+            let res = await fetch("https://swapi.dev/api/people/?format=json");
             let data = await res.json();
+            //console.log(data);
             setState({
                 ...state, 
-                people: data
-            })
+                people: data.results
+            })            
         }
         fetchPeople();
     });      
     
 
-    return(
-        
-        
-        <div className="container">
-                    <div className="row">
-                    
-                    {people.map(
-                        ({name, birth_year, hair_color, eye_color, mass }) => (
-                            <Character
-                            key={name}
-                            name={name}
-                            birth_year={birth_year}
-                            hair_color={hair_color}
-                            eye_color={eye_color}
-                            mass={mass}
-                            />
-                        )
-                    )}
-                    </div>
-                </div>
+    return (
+      <div className="container">
+        <div className="row">
+          {people.map(({ name, birth_year, hair_color, eye_color, mass }) => (
+            <Character
+              key={name}
+              name={name}
+              birth_year={birth_year}
+              hair_color={hair_color}
+              eye_color={eye_color}
+              mass={mass}
+            />
+          ))}
+        </div>
+      </div>
     );
 };
 export default withRouter(Characters);
